25ca752147c8aad12cfec7bde5113ae06058f846
ziagrosvenor <ziagrosvenor@live.co.uk>
2016-04-02 17:10:44 +0100

    Add all deps for deployment

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
-var io = require("socket.io")(3009)
+var express = require('express');
+var cors = require('cors');
 var crypto = require("crypto")
 var clients = []
+var app = express()
+
+var whitelist = ["http://0.0.0.0:8000"];
+var corsOptions = {
+  origin: function(origin, callback){
+    var originIsWhitelisted = whitelist.indexOf(origin) !== -1;
+    callback(null, originIsWhitelisted);
+  }
+};
+
+app.use(cors(corsOptions))
+app.use(express.static(__dirname + '/'));
+var http = require('http').Server(app)
+var io = require("socket.io")(http)
 
 io.on("connection", function(client) {
   client.id = id()
@@ -22,3 +37,12 @@ function id() {
   return crypto.randomBytes(12).toString("hex")
 }
 
+app.get('/', function(request, response) {
+  fs.readFile(__dirname + "/index.html", "utf8",  function(err, file) {
+    console.log(err)
+    response.send(file)
+  })
+});
+
+
+http.listen(3009)
