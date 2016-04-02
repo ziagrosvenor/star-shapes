5a9528633e756d24df745d9314beef852bfbeeb7
ziagrosvenor <ziagrosvenor@live.co.uk>
2016-04-02 14:03:23 +0100

    Change view container size

--- Config.elm
+++ Config.elm
@@ -1,7 +1,7 @@
 module Config (..) where
 
-areaW = 1100
-areaH = 800
+areaW = 700
+areaH = 450
 serverUrl = "http://localhost:3009"
 enemySpeed = 170
 backgroundImageUrl = "http://media.indiedb.com/images/articles/1/152/151754/auto/stars.png"
