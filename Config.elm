7bd6de923f69027bbd656157c8239676d5c55c85
ziagrosvenor <ziagrosvenor@live.co.uk>
2016-04-02 21:48:55 +0100

    Fix server configuration to production env

--- Config.elm
+++ Config.elm
@@ -2,7 +2,7 @@ module Config (..) where
 
 areaW = 700
 areaH = 450
-serverUrl = "http://localhost:3009"
+serverUrl = "https://star-shapes.herokuapp.com"
 enemySpeed = 170
 backgroundImageUrl = "http://media.indiedb.com/images/articles/1/152/151754/auto/stars.png"
 rockImageUrl = "http://vignette2.wikia.nocookie.net/dumbway2sdie/images/3/3a/Asteroid2.png/revision/latest?cb=20141202091820"
