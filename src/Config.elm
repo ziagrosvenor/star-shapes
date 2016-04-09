ec88adc80c653d34ed80d69570ed0a9704455687
ziagrosvenor <ziagrosvenor@live.co.uk>
2016-04-09 18:13:04 +0100

    Move image files into the project

--- src/Config.elm
+++ src/Config.elm
@@ -4,5 +4,5 @@ areaW = 700
 areaH = 450
 serverUrl = "https://star-shapes.herokuapp.com"
 enemySpeed = 200
-backgroundImageUrl = "http://media.indiedb.com/images/articles/1/152/151754/auto/stars.png"
-rockImageUrl = "http://vignette2.wikia.nocookie.net/dumbway2sdie/images/3/3a/Asteroid2.png/revision/latest?cb=20141202091820"
+backgroundImageUrl = "./stars-bg.png"
+rockImageUrl = "./asteroid.png"
